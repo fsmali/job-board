e7f5229829e91ebb57455cdb6ef9ebf84940535b
Ali Dede <alidede@Alis-MacBook-Pro.local>
2026-05-19 13:28:47 +0100

    dublicate ap;plication errors, back-end errors, job applications controller create

--- job-board-frontend/src/pages/JobDetailsPage.jsx
+++ job-board-frontend/src/pages/JobDetailsPage.jsx
@@ -2,10 +2,11 @@ import axios from 'axios';
 import { useQuery, useMutation, useQueryClient } from '@tanstack/react-query';
 import { useParams, Link, useNavigate } from 'react-router-dom';
 import { useAuth } from '../context/AuthContext';
-import { use } from 'react';
+import { useState } from 'react';
 
 function JobDetailsPage() {
   const { id } = useParams();
+  const [message, setMessage] = useState('');
   const navigate = useNavigate();
   const queryClient = useQueryClient();
 
@@ -31,23 +32,74 @@ function JobDetailsPage() {
       navigate('/');
     },
   });
+  const applyMutation = useMutation({
+    mutationFn: async () => {
+      const { data } = await axios.post(
+        `http://localhost:3000/jobs/${id}/job_applications`,
+        {
+          job_application: {
+            message: message,
+          },
+        },
+        {
+          headers: {
+            Authorization: `Bearer ${token}`,
+          },
+        },
+      );
+      return data;
+    },
+    onSuccess: () => {
+      queryClient.invalidateQueries({ queryKey: ['jobs'] });
+      setMessage('');
+      navigate('/');
+    },
+  });
 
   if (isLoading) return <h1>Loading job...</h1>;
   if (isError) return <h1>Job not found</h1>;
 
   return (
-    <div>
-      <Link to="/">Back to jobs</Link>
-      <h1>{job.title}</h1>
-      <p>{job.description}</p>
-      <p>{job.location}</p>
-      <p>{job.category}</p>
-      <p>£{job.budget}</p>
-      <Link to={`/jobs/${job.id}/edit`}>
-        <button>Edit Job</button>
-      </Link>
-      <button onClick={() => deleteJobMutation.mutate()}>Delete Job</button>
-    </div>
+    <>
+      ≈
+      <div>
+        <Link to="/">Back to jobs</Link>
+        <h1>{job.title}</h1>
+        <p>{job.description}</p>
+        <p>{job.location}</p>
+        <p>{job.category}</p>
+        <p>£{job.budget}</p>
+        <Link to={`/jobs/${job.id}/edit`}>
+          <button>Edit Job</button>
+        </Link>
+        <button onClick={() => deleteJobMutation.mutate()}>Delete Job</button>
+      </div>
+      <form
+        onSubmit={(e) => {
+          e.preventDefault();
+          applyMutation.mutate();
+        }}
+      >
+        <h2>Apply this job</h2>
+        <textarea
+          placeholder=" write a short message about yourself"
+          value={message}
+          onChange={(e) => setMessage(e.target.value)}
+        ></textarea>
+        <div>
+          <button type="submit" disabled={applyMutation.isPending}>
+            {applyMutation.isPending ? 'Applying' : 'Apply'}
+          </button>
+          {applyMutation.isError && (
+            <p>
+              {applyMutation.error.data?.error ||
+                applyMutation.error.data?.errors?.[0] ||
+                'You have already applied this job'}
+            </p>
+          )}
+        </div>
+      </form>
+    </>
   );
 }
 
